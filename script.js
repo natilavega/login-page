c7d320d27fa9deb5754514f55a4d32719ce82755
Natalia <35309830+natilavega@users.noreply.github.com>
2019-05-24 01:17:38 +0200

    add core files

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function(){
+
+	$('#login-switch').click(function(){
+		$('#login').fadeOut(500).promise().done(function(){
+			$('#signup').fadeIn(500);
+		});
+	});
+
+	$('#signup-switch').click(function(){
+		$('#signup').fadeOut(500).promise().done(function(){
+			$('#login').fadeIn(500);	
+		});
+	});
+
+	$('#restore-password').click(function(){
+		$('#login').fadeOut(500).promise().done(function(){
+			$('#restore').fadeIn(500);	
+		});
+	});
+});
